986c480d2f8e6c81a98af36ef0b26080dc14daa0
Brendan Metcalfe <bgmetcalfe@gmail.com>
2016-12-05 00:04:10 -0500

    simple lightbox closes

--- logic.js
+++ logic.js
@@ -1,5 +1,11 @@
 function myOverFunction() {
     console.log("Yoo");
     var bigPanel = document.getElementById("expanded-panel");
-	bigPanel.style.display = "inline";  
+	bigPanel.style.display = "inline"; 
+
+	window.onclick = function(event) {
+	    if (event.target == bigPanel) {
+	        bigPanel.style.display = "none";
+	    }
+	} 
 }
